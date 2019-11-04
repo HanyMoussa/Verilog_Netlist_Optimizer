567707fa36fa7027090f74f37ad05aed1190b555
ramezmoussa <57135988+ramezmoussa@users.noreply.github.com>
2019-11-04 16:17:18 +0100

    Fixed the Extrapolation Module

--- Extract Capacitance.py
+++ Extract Capacitance.py
@@ -7,16 +7,16 @@ from scipy.interpolate import UnivariateSpline
 def extractCapacitance(capacitances, delays, target):
     if((target > capacitances[-1]) | (target < capacitances[0])):
         extrapolator = UnivariateSpline(capacitances, delays)
-        return extrapolator([target])
+        return max(extrapolator([target]), 0)
     else:
         interpolateDelay = scipy.interpolate.interp1d(capacitances, delays)
-        return interpolateDelay(target)
+        return max(interpolateDelay(target), 0)
     
     
 def main():
     capacitances = [20, 30, 40, 50, 80]
     delays = [10, 20, 30, 35, 50]
-    targetCapacitance = 95
+    targetCapacitance = 10
     
     print(extractCapacitance(capacitances,delays, targetCapacitance))
     
--- GraphStructure.py
+++ GraphStructure.py
@@ -6,14 +6,20 @@
 from collections import defaultdict
 
 graph = defaultdict(list);
-graph['u7'].append(('u5', 2.64))
-graph['u7'].append(('u8', 2.64));
-graph['u5'].append(('u8', 1))
 
-#print(graph)
+# the key is the source node
+# ('YS', u5, 'A', 2.64):
+#   We go from pin YS in u7 to pin A in u5 and the delay is 2.64
+graph['u7'].append(('YS', 'u5', 'A', 2.64))
+graph['u7'].append(('YC', 'u8', 'B', 2.5))
+graph['u5'].append(('Y', 'u8', 'C', 1))
+
+
+
+# Construct the Graph from the wires dictionary:    
 
 #print(graph['u7'][0][1])
 
 for key,value in graph.items():
     for edge in value:
-        print ("Vertex:", key, "to",  edge[0], "weight = ", edge[1])
+        print ("Source:", key, "from pin",  edge[0], "to cell", edge[1], "pin", edge[2], "with weight = ", edge[3])
