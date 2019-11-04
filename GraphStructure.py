be082838986e478ebee2a4ed7be8c1e10376423e
ramezmoussa <57135988+ramezmoussa@users.noreply.github.com>
2019-11-04 11:42:00 +0100

    Create GraphStructure.py
    A module to represent and test the Graph struture that would be used to represent the gate level netlist.

---
+++ GraphStructure.py
@@ -0,0 +1,15 @@
+@author: Ramez Moussa
+
+# Creating the Graph Structure that will represent the circuit
+# A node represents a cell and all the outgoing edges represent the delay of that cell
+
+from collections import defaultdict
+
+graph = defaultdict(list);
+graph['u7'].append(('u5', 2.64))
+graph['u7'].append(('u8', 2.64));
+graph['u5'].append(('u8', 1))
+
+print(graph)
+
+print(graph['u7'][0][1])
