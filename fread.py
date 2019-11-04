e0acd0dd6b0511129e0b48d51b41191dd8f21a5c
HanyMoussa <57130480+HanyMoussa@users.noreply.github.com>
2019-11-04 14:12:55 +0200

    Parse Gate Level Netlist (1)
    In this version, we parse the instantiations of the cells and print on the screen: the cell name, the instantiation name, the pins and the wires connected to them

--- fread.py
+++ fread.py
@@ -1,4 +1,21 @@
 
 f = open("netlist.v", "r")
-print(f.read())
+for line in f:
+    print('\n')
+   # print(line)
+    data = line.split()
+    count = 0
+    for x in data:
+        if(count == 0):
+            cellName = x;
+            print("Cell name: ", cellName)
+        elif(count == 1):
+            instanceName = x;
+            print("Instance name: ", instanceName)
+        elif((count > 2) & (count <= len(data) - 2)): #ignore data[2] entry as it is an empty bracket
+            if(x != data[len(data)-2]):
+                print("the", x[1], "pin", x[3:len(x)-2])
+            else:
+                print("the", x[1], "pin", x[3:len(x)-1])
+        count += 1;
 f.close()
